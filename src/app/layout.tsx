ee4362cc497136f0e1bf198f87b135acc5df9066
shreekantswami <shreekantswami@gmail.com>
2026-04-26 17:10:51 +0530

    fix : Fixed the OG Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/assets/OG_IMAGE.png",
-        width: 1536,
-        height: 1024,
+        width: 1200,
+        height: 630,
         alt: `${siteConfig.name} social preview`,
       },
     ],
